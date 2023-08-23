4d9df9111d187ef0836d9f298032c95bb88d68e5
Ishan Khandelwal <ishankhandelwals@gmail.com>
2023-08-23 12:46:45 +0530

    corrected date format

--- app/page.tsx
+++ app/page.tsx
@@ -111,6 +111,7 @@ export default function Home() {
                 onChange={(date) => {
                   setDate(date);
                 }}
+                format="DD/MM/YYYY"
               />
             </LocalizationProvider>
             <div
